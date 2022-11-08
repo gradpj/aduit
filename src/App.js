031ac0633859458f316b56376bbf3b274c68570e
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-11-08 14:33:24 +0900

    📑 :: App.js 생성
    📑 :: App.js 생성

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Title from 'pages/TitlePage/Title';
+
+const App = () => {
+	return (
+		<div className='App' style={{width:"100%", height:"100%"}}>
+           <Title />
+		</div>
+	);
+}
+
+export default App;
