48bfe75b74543e27f7e57ef887215bd641fcf1bf
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-11-08 14:40:05 +0900

    ⚡️ :: Title 페이지 수정
    ⚡️ :: Title 페이지 수정

--- src/pages/TitlePage/Title.js
+++ src/pages/TitlePage/Title.js
@@ -1,17 +1,18 @@
 import styles from './Title.module.css';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
+import { IconButton } from '@material-ui/core';
 
 const Title = () => {
   return (
-    <div>
-        <body>
-          <div className={styles.wrapper}>
-            <h1>aduit</h1>
-            <h2>action detector using inferred tiling</h2>
-            <ArrowDropDownIcon fontSize='large' color='secondary' />
+        <div className={styles.titleBody}>
+          <div className={styles.titleWrapper}>
+            <h1 className={styles.titleH1}>aduit</h1>
+            <h2 className={styles.titleH2}>action detector using inferred tiling</h2>
+            <IconButton>
+              <ArrowDropDownIcon style={{ fontSize : '100', color : '#CACACA'}} />
+            </IconButton>
           </div>
-        </body>
-    </div>
+        </div>
   );
 };
 
