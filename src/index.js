f73061e007f864607e82facae6402d84556a080d
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-11-08 14:39:50 +0900

    ⚡️ :: index.js 수정
    ⚡️ :: index.js 수정

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import Title from 'pages/TitlePage/Title';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App'
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Title />
+    <React.StrictMode style={{width:"100%", height:"100%"}}>
+        <App />
     </React.StrictMode>,
     document.getElementById('root')
 );
