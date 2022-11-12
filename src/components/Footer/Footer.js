b5654890b567aaee1b8a6981f6f1c9e9cb1cb2a7
2kkeullim <rosel0314@mail.hongik.ac.kr>
2022-11-12 23:54:56 +0900

    📑 :: Footer 레이아웃 추가
    📑 :: Footer 레이아웃 추가

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+    return (
+        <footer className={styles.footer}>
+            <div className={styles.contents}>
+                <div className={styles.logo}>
+                    aduit
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
